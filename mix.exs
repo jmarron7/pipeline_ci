c6d9d34ac25085bdb0caed7420aad8fa0fa007f5
Jesus Marron <jesusmarron7@gmail.com>
2023-09-25 16:24:45 -0500

    added dialyxir

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule PipelineCi.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
   end
 
